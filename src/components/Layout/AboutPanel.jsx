f58972f1eecbf012f926903a968f5b3325e72648
Luke4211 <Lureworld20>
2022-05-03 18:47:53 -0400

    Just in case I change my mind on the linkedin badge

--- src/components/Layout/AboutPanel.jsx
+++ src/components/Layout/AboutPanel.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AboutIcon from '../../assets/test.jpg';
 import './AboutPanel.css';
 
 function AboutPanel() {
+  useEffect(() => {
+    const linkedInScript = document.createElement('script');
+    linkedInScript.src = 'https://platform.linkedin.com/badges/js/profile.js';
+    linkedInScript.async = true;
+    linkedInScript.defer = true;
+
+    document.body.appendChild(linkedInScript);
+    return () => {
+      document.body.removeChild(linkedInScript);
+    };
+  }, []);
+
   return (
-    <div className="about_container" />
+    <div className="about_container">
+      <img className="about_img" src={AboutIcon} alt="About" />
+      <div className="about_name">
+
+        <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="lucas-gorski-09686b173" data-version="v1"><a className="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/lucas-gorski-09686b173?trk=profile-badge">{}</a></div>
+
+      </div>
+    </div>
   );
 }
 
